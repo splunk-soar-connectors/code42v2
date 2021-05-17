54997d618282f2e0ad9670fd6e75a9d4b8570d9f
Peter Briggs <peter.briggs@code42.com>
2021-05-17 11:18:19 -0500

    Rename "phantom-stubs" to "phantomstubs", move test code to `tests` directory, add contributing docs

--- phantomstubs/setup.py
+++ phantomstubs/setup.py
@@ -1,8 +1,8 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="phantom-stubs",
-    version="0.0.1",
+    name="phantomstubs",
+    version="1.0.0",
     description="Stubs of Phantom modules and methods to facilitate testing",
     packages=find_packages()
 )
