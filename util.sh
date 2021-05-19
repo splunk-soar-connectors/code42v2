5c8b3e2e1966c7cee7146d368b997827b6346b9c
Juliya Smith <juliya.smith@code42.com>
2021-05-19 12:34:39 +0000

    adjust util

--- util.sh
+++ util.sh
@@ -1,25 +1,49 @@
 #!/bin/bash
 
-set -e
+set -eou pipefail
+
+make_tar() {
+  echo "Tarring the ball..."
+  pushd .. && tar -cvf phcode42v2/phcode42v2.tgz -X phcode42v2/exclude_files.txt phcode42v2/* && popd
+}
+
+clean() {
+  rm -f phcode42v2.tgz
+}
+
+deploy() {
+  echo "Moving to remote..."
+  sshpass -p "${PHANTOM_VM_PASSWORD}" scp phcode42v2.tgz "phantom@${PHANTOM_VM_IP_ADDR}":/home/phantom
+  echo "Untarring on remote..."
+  sshpass -p "${PHANTOM_VM_PASSWORD}" ssh phantom@${PHANTOM_VM_IP_ADDR} \
+		"rm -rf phcode42v2 && tar -xvf phcode42v2.tgz && cd phcode42v2 && phenv python /opt/phantom/bin/compile_app.pyc -i"
+}
+
+print_usage() {
+  echo "./util.sh <tar, clean, deploy, open-web>"
+}
 
 main() {
   case $1 in
   "")
-    echo "Kindly supply a command" && exit 0
+    echo "Kindly supply a command" && print_usage && exit 0
+    ;;
+  tar)
+    clean
+    make_tar
+    ;;
+  clean)
+    clean
     ;;
   deploy)
-    echo "Tarring the ball..."
-    pushd .. && tar -cvf phcode42v2/phcode42v2.tgz -X phcode42v2/exclude_files.txt phcode42v2/* && popd
-    echo "Moving to remote..."
-    sshpass -p "${PHANTOM_VM_PASSWORD}" scp phcode42v2.tgz "phantom@${PHANTOM_VM_IP_ADDR}":/home/phantom
-    echo "Untarring on remote..."
-    sshpass -p "${PHANTOM_VM_PASSWORD}" ssh phantom@${PHANTOM_VM_IP_ADDR} "rm -rf phcode42v2 && tar -xvf phcode42v2.tgz && cd phcode42v2 && phenv python /opt/phantom/bin/compile_app.pyc -i"
+    make_tar
+    deploy
     ;;
   open-web)
     open https://$PHANTOM_VM_IP_ADDR:9999
     ;;
   *)
-    echo "Not a valid command" && exit 0
+    echo "Not a valid command" && print_usage && exit 0
   esac
 }
 
