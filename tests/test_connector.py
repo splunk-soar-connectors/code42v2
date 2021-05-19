4f31cb310225668ee6046c3fcb27607b466d7ed7
Peter Briggs <peter.briggs@code42.com>
2021-05-19 16:15:03 -0500

    Respond PR feedback

--- tests/test_connector.py
+++ tests/test_connector.py
@@ -22,9 +22,8 @@ def mock_py42_client(mocker):
 
 
 @fixture
-def connector(mock_py42_client):
+def connector():
     connector = code42_connector.Code42Connector()
-    connector._client = mock_py42_client
     return connector
 
 
@@ -45,25 +44,28 @@ def test_connectivity_connector(mock_py42_client):
 
 
 class TestCode42Connector(object):
-    def test_handle_connectivity(self, mocker, connector):
+    def test_handle_test_connectivity(self, mocker, mock_py42_client, connector):
         param = {}
         mocker.patch("phantom.base_connector.BaseConnector.add_action_result", return_value=ActionResult(dict(param)))
+        mocker.patch("py42.sdk.from_local_account", return_value=mock_py42_client)
         set_status = mocker.patch("phantom.action_result.ActionResult.set_status")
 
         connector._handle_test_connectivity(param)
 
         set_status.assert_called_with(phantom.APP_SUCCESS)
 
-    def test_handle_test_connectivity_calls_users_get_current(self, mocker, connector):
+    def test_handle_test_connectivity_calls_users_get_current(self, mocker, mock_py42_client, connector):
         param = {}
+        mocker.patch("py42.sdk.from_local_account", return_value=mock_py42_client)
         mocker.patch("phantom.base_connector.BaseConnector.add_action_result", return_value=ActionResult(dict(param)))
 
         connector._handle_test_connectivity(param)
 
         connector._client.users.get_current.assert_called_with()
 
-    def test_handle_connectivity_sets_error_status_if_sdk_throws_exception(self, mocker, connector, mock_py42_client):
+    def test_handle_connectivity_sets_error_status_if_sdk_throws_exception(self, mocker, mock_py42_client, connector):
         param = {}
+        mocker.patch("py42.sdk.from_local_account", return_value=mock_py42_client)
         mocker.patch("phantom.base_connector.BaseConnector.add_action_result", return_value=ActionResult(dict(param)))
         mock_py42_client.users.get_current.side_effect = Py42UnauthorizedError(mock.Mock(status=401))
         set_status = mocker.patch("phantom.action_result.ActionResult.set_status")
@@ -72,9 +74,11 @@ class TestCode42Connector(object):
 
         set_status.assert_called_with(phantom.APP_ERROR, mocker.ANY)
 
-    def test_handle_action_calls_handle_connectivity(self, mocker, test_connectivity_connector):
+    def test_handle_action_calls_handle_connectivity(self, mocker, mock_py42_client, test_connectivity_connector):
         def fake_set_status(self, status, message=None):
             return status == phantom.APP_SUCCESS
+
+        mocker.patch("py42.sdk.from_local_account", return_value=mock_py42_client)
         mocker.patch("phantom.action_result.ActionResult.set_status", new=fake_set_status)
         param = {}
         mocker.patch("phantom.base_connector.BaseConnector.add_action_result", return_value=ActionResult(dict(param)))
@@ -83,13 +87,14 @@ class TestCode42Connector(object):
 
         assert ret == phantom.APP_SUCCESS
 
-    def test_initialize_reads_credentials_from_config_and_creates_py42_client_from_local_account(self, mocker,
-                                                                                                 mock_py42_client):
+    def test_initialize_reads_credentials_from_config(self, mocker,
+                                                      mock_py42_client):
         connector = code42_connector.Code42Connector()
         config = {"cloud_instance": "http://cloud.code42.com", "username": "user.name", "password": "password123!"}
         mocker.patch("phantom.base_connector.BaseConnector.get_config", return_value=config)
-        from_local_account = mocker.patch("py42.sdk.from_local_account")
 
         connector.initialize()
 
-        from_local_account.assert_called_with(config["cloud_instance"], config["username"], config["password"])
+        assert connector._cloud_instance == config["cloud_instance"]
+        assert connector._username == config["username"]
+        assert connector._password == config["password"]
