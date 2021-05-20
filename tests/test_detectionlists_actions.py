743ea8e37c8a11490216d90689e879080bc6db4a
Juliya Smith <juliya.smith@code42.com>
2021-05-20 19:44:03 +0000

    output and tests

--- tests/test_detectionlists_actions.py
+++ tests/test_detectionlists_actions.py
@@ -1,4 +1,5 @@
 import json
+from pytest import fixture
 
 from py42.response import Py42Response
 from requests import Response
@@ -7,38 +8,112 @@ from phantom.action_result import ActionResult
 from tests.conftest import create_fake_connector
 
 
+_TEST_USER_UID = "TEST_USER_UID"
+
+
+@fixture
+def mock_py42_with_user(mocker, mock_py42_client):
+    http_response = mocker.MagicMock(spec=Response)
+    http_response.text = json.dumps({"users": [{"userUid": _TEST_USER_UID}]})
+    mock_py42_client.users.get_by_username.return_value = Py42Response(http_response)
+    return mock_py42_client
+
+
 class TestCode42DetectionListsConnector(object):
 
     def test_handle_action_when_add_departing_employee_calls_add_with_expected_args(
-        self, mocker, mock_py42_client, mock_result_adder
+        self, mock_py42_with_user, mock_result_adder
     ):
         param = {"username": "test@example.com", "departure_date": "2030-01-01"}
         result = ActionResult(dict(param))
-        result.update_summary = mocker.MagicMock()
         mock_result_adder.return_value = result
-        http_response = mocker.MagicMock(spec=Response)
-        http_response.text = json.dumps({"users": [{"userUid": "TEST_USER_UID"}]})
-        mock_py42_client.users.get_by_username.return_value = Py42Response(http_response)
         connector = create_fake_connector("add_departing_employee")
-        connector._client = mock_py42_client
+        connector._client = mock_py42_with_user
         connector.handle_action(param)
-        mock_py42_client.detectionlists.departing_employee.add.assert_called_once_with(
+        mock_py42_with_user.detectionlists.departing_employee.add.assert_called_once_with(
             "TEST_USER_UID", departure_date="2030-01-01"
         )
 
+    def test_handle_action_when_add_departing_employee_adds_user_id_to_summary(
+        self, mocker, mock_py42_with_user, mock_result_adder
+    ):
+        param = {"username": "test@example.com", "departure_date": "2030-01-01"}
+        result = ActionResult(dict(param))
+        update_summary_mock = mocker.MagicMock()
+        result.update_summary = update_summary_mock
+        mock_result_adder.return_value = result
+        connector = create_fake_connector("add_departing_employee")
+        connector._client = mock_py42_with_user
+        connector.handle_action(param)
+        update_summary_mock.assert_called_once_with({"userId": _TEST_USER_UID})
+
+    def test_handle_action_when_add_departing_employee_adds_response_to_data(
+        self, mocker, mock_py42_with_user, mock_result_adder
+    ):
+        param = {"username": "test@example.com", "departure_date": "2030-01-01"}
+        response_data = {
+            "type$": "DEPARTING_EMPLOYEE_V2",
+            "tenantId": "11114444-2222-3333-4444-666634888863",
+            "userId": _TEST_USER_UID,
+            "userName": "test@example.com",
+            "displayName": "",
+            "createdAt": "2021-05-20T19:40:14.8909434Z",
+            "status": "OPEN",
+            "cloudUsernames": [
+                "test@example.com"
+            ]
+        }
+        response = mocker.MagicMock(spec=Response)
+        response.text = json.dumps(response_data)
+        mock_py42_with_user.detectionlists.departing_employee.add.return_value = Py42Response(response)
+        result = ActionResult(dict(param))
+        add_data_mock = mocker.MagicMock()
+        result.add_data = add_data_mock
+        mock_result_adder.return_value = result
+        connector = create_fake_connector("add_departing_employee")
+        connector._client = mock_py42_with_user
+        connector.handle_action(param)
+        add_data_mock.assert_called_once_with(response_data)
+
     def test_handle_action_when_remove_departing_employee_calls_remove_with_expected_args(
-        self, mocker, mock_py42_client, mock_result_adder
+        self, mock_py42_with_user, mock_result_adder
     ):
         param = {"username": "test@example.com"}
         result = ActionResult(dict(param))
-        result.update_summary = mocker.MagicMock()
         mock_result_adder.return_value = result
-        http_response = mocker.MagicMock(spec=Response)
-        http_response.text = json.dumps({"users": [{"userUid": "TEST_USER_UID"}]})
-        mock_py42_client.users.get_by_username.return_value = Py42Response(http_response)
         connector = create_fake_connector("remove_departing_employee")
-        connector._client = mock_py42_client
+        connector._client = mock_py42_with_user
         connector.handle_action(param)
-        mock_py42_client.detectionlists.departing_employee.remove.assert_called_once_with(
+        mock_py42_with_user.detectionlists.departing_employee.remove.assert_called_once_with(
             "TEST_USER_UID"
         )
+
+    def test_handle_action_when_remove_departing_employee_adds_user_id_to_summary(
+        self, mocker, mock_py42_with_user, mock_result_adder
+    ):
+        param = {"username": "test@example.com", "departure_date": "2030-01-01"}
+        result = ActionResult(dict(param))
+        update_summary_mock = mocker.MagicMock()
+        result.update_summary = update_summary_mock
+        mock_result_adder.return_value = result
+        connector = create_fake_connector("remove_departing_employee")
+        connector._client = mock_py42_with_user
+        connector.handle_action(param)
+        update_summary_mock.assert_called_once_with({"userId": _TEST_USER_UID})
+
+    def test_handle_action_when_remove_departing_employee_adds_response_to_data(
+        self, mocker, mock_py42_with_user, mock_result_adder
+    ):
+        param = {"username": "test@example.com", "departure_date": "2030-01-01"}
+        response_data = {}
+        response = mocker.MagicMock(spec=Response)
+        response.text = json.dumps(response_data)
+        mock_py42_with_user.detectionlists.departing_employee.remove.return_value = Py42Response(response)
+        result = ActionResult(dict(param))
+        add_data_mock = mocker.MagicMock()
+        result.add_data = add_data_mock
+        mock_result_adder.return_value = result
+        connector = create_fake_connector("remove_departing_employee")
+        connector._client = mock_py42_with_user
+        connector.handle_action(param)
+        add_data_mock.assert_called_once_with(response_data)
