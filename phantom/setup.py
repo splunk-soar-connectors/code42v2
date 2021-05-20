1554e3d7db827ba88ecc735769286bf586fa062c
Juliya Smith <juliya.smith@code42.com>
2021-05-20 15:55:21 +0000

    rm setuppy for stubs

--- phantom/setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import find_packages, setup
-
-setup(
-    name="phantom",
-    version="1.0.0",
-    description="Stubs of Phantom modules and methods to facilitate testing",
-    packages=find_packages()
-)
