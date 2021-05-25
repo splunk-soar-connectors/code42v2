4cd95cf9d12b41c9baed7283bd5b5f2c283cd18e
Tim Abramson <tim.abramson@code42.com>
2021-05-25 12:49:55 -0500

    action_key decorator and generate password

--- code42_connector.py
+++ code42_connector.py
@@ -2,6 +2,8 @@
 from __future__ import print_function, unicode_literals
 
 import json
+import random
+import string
 
 import py42.sdk
 import requests
@@ -17,16 +19,18 @@ class RetVal(tuple):
         return tuple.__new__(RetVal, (val1, val2))
 
 
-class Code42Connector(BaseConnector):
-    TEST_CONNECTIVITY_ACTION_ID = "test_connectivity"
-    ADD_DEPARTING_EMPLOYEE_ACTION_ID = "add_departing_employee"
-    REMOVE_DEPARTING_EMPLOYEE_ACTION_ID = "remove_departing_employee"
-    CREATE_USER = "create_user"
-    BLOCK_USER = "block_user"
-    UNBLOCK_USER = "unblock_user"
-    DEACTIVATE_USER = "deactivate_user"
-    REACTIVATE_USER = "reactivate_user"
+ACTION_MAP = {}
 
+
+def action_key(key):
+    def wrapper(f):
+        ACTION_MAP[key] = f
+        return f
+
+    return wrapper
+
+
+class Code42Connector(BaseConnector):
     def __init__(self):
         super(Code42Connector, self).__init__()
 
@@ -35,22 +39,6 @@ class Code42Connector(BaseConnector):
         self._username = None
         self._password = None
         self._client = None
-        self._action_map = {
-            self.TEST_CONNECTIVITY_ACTION_ID: lambda x: self._handle_test_connectivity(
-                x
-            ),
-            self.ADD_DEPARTING_EMPLOYEE_ACTION_ID: lambda x: self._handle_add_departing_employee(
-                x
-            ),
-            self.REMOVE_DEPARTING_EMPLOYEE_ACTION_ID: lambda x: self._handle_remove_departing_employee(
-                x
-            ),
-            self.CREATE_USER: lambda x: self._handle_create_user(x),
-            self.BLOCK_USER: lambda x: self._handle_block_user(x),
-            self.UNBLOCK_USER: lambda x: self._handle_unblock_user(x),
-            self.DEACTIVATE_USER: lambda x: self._handle_deactivate_user(x),
-            self.REACTIVATE_USER: lambda x: self._handle_reactivate_user(x),
-        }
 
     @property
     def client(self):
@@ -76,9 +64,10 @@ class Code42Connector(BaseConnector):
     def handle_action(self, param):
         action_id = self.get_action_identifier()
         self.debug_print("action_id", action_id)
-        action = self._action_map[action_id]
-        return action(param)
+        action = ACTION_MAP[action_id]
+        return action(self, param)
 
+    @action_key("test_connectivity")
     def _handle_test_connectivity(self, param):
         # Add an action result object to self (BaseConnector) to represent the action for this param
         action_result = self.add_action_result(ActionResult(dict(param)))
@@ -98,6 +87,7 @@ class Code42Connector(BaseConnector):
         self.save_progress("Test Connectivity Passed")
         return action_result.set_status(phantom.APP_SUCCESS)
 
+    @action_key("add_departing_employee")
     def _handle_add_departing_employee(self, param):
         self._log_action_handler()
         action_result = self._add_action_result(param)
@@ -117,6 +107,7 @@ class Code42Connector(BaseConnector):
         status_message = f"{username} was added to the departing employee list"
         return action_result.set_status(phantom.APP_SUCCESS, status_message)
 
+    @action_key("remove_departing_employee")
     def _handle_remove_departing_employee(self, param):
         self._log_action_handler()
         action_result = self._add_action_result(param)
@@ -128,23 +119,20 @@ class Code42Connector(BaseConnector):
         status_message = f"{username} was removed from the departing employee list"
         return action_result.set_status(phantom.APP_SUCCESS, status_message)
 
+    @action_key("create_user")
     def _handle_create_user(self, param):
         self._log_action_handler()
         action_result = self._add_action_result(param)
         username = param["username"]
-        org_uid = param["org_uid"]
-        password = param.get("password")
-        firstname = param.get("firstname")
-        lastname = param.get("lastname")
-        notes = param.get("notes")
+        password = param.get("password") or _generate_password()
         response = self.client.users.create_user(
-            org_uid=org_uid,
+            org_uid=param["org_uid"],
             username=username,
             email=username,
             password=password,
-            first_name=firstname,
-            last_name=lastname,
-            notes=notes,
+            first_name=param.get("first_name"),
+            last_name=param.get("last_name"),
+            notes=param.get("notes"),
         )
         user_id = response["userUid"]
         action_result.add_data(response.data)
@@ -153,6 +141,7 @@ class Code42Connector(BaseConnector):
             phantom.APP_SUCCESS, f"{username} was created with user_id: {user_id}"
         )
 
+    @action_key("block_user")
     def _handle_block_user(self, param):
         self._log_action_handler()
         action_result = self._add_action_result(param)
@@ -163,6 +152,7 @@ class Code42Connector(BaseConnector):
         action_result.update_summary({"user_id": user["userUid"], "username": username})
         return action_result.set_status(phantom.APP_SUCCESS, f"{username} was blocked")
 
+    @action_key("unblock_user")
     def _handle_unblock_user(self, param):
         self._log_action_handler()
         action_result = self._add_action_result(param)
@@ -175,6 +165,7 @@ class Code42Connector(BaseConnector):
             phantom.APP_SUCCESS, f"{username} was unblocked"
         )
 
+    @action_key("deactivate_user")
     def _handle_deactivate_user(self, param):
         self._log_action_handler()
         action_result = self._add_action_result(param)
@@ -187,6 +178,7 @@ class Code42Connector(BaseConnector):
             phantom.APP_SUCCESS, f"{username} was deactivated"
         )
 
+    @action_key("reactivate_user")
     def _handle_reactivate_user(self, param):
         self._log_action_handler()
         action_result = self._add_action_result(param)
@@ -289,6 +281,17 @@ def main():
         print(json.dumps(json.loads(ret_val), indent=4))
 
     exit(0)
+
+
+def _generate_password():
+    upper = random.choices(string.ascii_uppercase, k=5)
+    lower = random.choices(string.ascii_lowercase, k=5)
+    numbers = random.choices(string.digits, k=5)
+    special = random.choices(string.punctuation, k=5)
+    chars = upper + lower + numbers + special
+    for _ in range(random.choice(range(1000))):
+        random.shuffle(chars)
+    return "".join(chars)
 
 
 if __name__ == "__main__":
