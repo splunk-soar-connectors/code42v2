c6437be29648b4439459ec3c1f5181cff3d3be82
Alan Grgic <alan.grgic@code42.com>
2021-05-24 21:23:35 -0500

    rm unused stub stuff

--- src/phantom/app.py
+++ src/phantom/app.py
@@ -1,10 +1,2 @@
 APP_ERROR = False
 APP_SUCCESS = True
-APP_SUCCESS_STR = 'success'
-APP_ERROR_STR = 'failed'
-
-def get_succ_or_failure_text(status_code):
-    if not status_code:
-        return APP_ERROR_STR
-    else:
-        return APP_SUCCESS_STR
