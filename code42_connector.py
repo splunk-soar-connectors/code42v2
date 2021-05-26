059ac580807653a1e4f27ac52d3714aca112eb1c
Peter Briggs <peter.briggs@code42.com>
2021-05-26 13:27:55 -0500

    Implement set state

--- code42_connector.py
+++ code42_connector.py
@@ -19,7 +19,6 @@ from phantom.base_connector import BaseConnector
 
 
 class RetVal(tuple):
-
     def __new__(cls, val1, val2=None):
         return tuple.__new__(RetVal, (val1, val2))
 
@@ -36,7 +35,6 @@ def action_handler_for(key):
 
 
 class Code42Connector(BaseConnector):
-
     def __init__(self):
         super(Code42Connector, self).__init__()
 
@@ -65,11 +63,15 @@ class Code42Connector(BaseConnector):
         action_result = self.add_action_result(ActionResult(dict(param)))
 
         if not action_handler:
-            return action_result.set_status(phantom.APP_ERROR, f"Code42: Action {action_id} does not exist.")
+            return action_result.set_status(
+                phantom.APP_ERROR, f"Code42: Action {action_id} does not exist."
+            )
 
         try:
             if not self._client:
-                self._client = py42.sdk.from_local_account(self._cloud_instance, self._username, self._password)
+                self._client = py42.sdk.from_local_account(
+                    self._cloud_instance, self._username, self._password
+                )
             self.save_progress(f"Code42: handling action {action_id}...")
             return action_handler(self, param, action_result)
         except Exception as ex:
@@ -89,7 +91,9 @@ class Code42Connector(BaseConnector):
         username = param["username"]
         departure_date = param.get("departure_date")
         user_id = self._get_user_id(username)
-        response = self._client.detectionlists.departing_employee.add(user_id, departure_date=departure_date)
+        response = self._client.detectionlists.departing_employee.add(
+            user_id, departure_date=departure_date
+        )
 
         note = param.get("note")
         if note:
@@ -111,7 +115,9 @@ class Code42Connector(BaseConnector):
     @action_handler_for("list_departing_employees")
     def _handle_list_departing_employees(self, param, action_result):
         filter_type = param.get("filter_type", DepartingEmployeeFilters.OPEN)
-        results_generator = self._client.detectionlists.departing_employee.get_all(filter_type=filter_type)
+        results_generator = self._client.detectionlists.departing_employee.get_all(
+            filter_type=filter_type
+        )
 
         page = None
         for page in results_generator:
@@ -146,7 +152,9 @@ class Code42Connector(BaseConnector):
     @action_handler_for("list_highrisk_employees")
     def _handle_list_high_risk_employees(self, param, action_result):
         filter_type = param.get("filter_type", HighRiskEmployeeFilters.OPEN)
-        results_generator = self._client.detectionlists.high_risk_employee.get_all(filter_type=filter_type)
+        results_generator = self._client.detectionlists.high_risk_employee.get_all(
+            filter_type=filter_type
+        )
 
         page = None
         for page in results_generator:
@@ -158,7 +166,7 @@ class Code42Connector(BaseConnector):
         action_result.update_summary({"total_count": total_count})
         return action_result.set_status(phantom.APP_SUCCESS)
 
-    '''ALERTS ACTIONS'''
+    """ALERTS ACTIONS"""
 
     @action_handler_for("get_alert_details")
     def _handle_get_alert_details(self, param, action_result):
@@ -166,7 +174,9 @@ class Code42Connector(BaseConnector):
         response = self._client.alerts.get_details([alert_id])
         alert = response.data["alerts"][0]
         action_result.add_data(alert)
-        action_result.update_summary({"username": alert["actor"], "user_id": alert["actorId"]})
+        action_result.update_summary(
+            {"username": alert["actor"], "user_id": alert["actorId"]}
+        )
         return action_result.set_status(phantom.APP_SUCCESS)
 
     @action_handler_for("search_alerts")
@@ -176,21 +186,46 @@ class Code42Connector(BaseConnector):
         end_date = param.get("end_date")
         alert_state = param.get("alert_state")
 
-        if username is None and start_date is None and end_date is None and alert_state is None:
-            return action_result.set_status(phantom.APP_ERROR, "Must supply a search term.")
+        if (
+            username is None
+            and start_date is None
+            and end_date is None
+            and alert_state is None
+        ):
+            return action_result.set_status(
+                phantom.APP_ERROR, "Must supply a search term."
+            )
 
         if not self._validate_date_range(start_date, end_date):
-            return action_result.set_status(phantom.APP_ERROR, "Start Date and End Date are both required to search by "
-                                                               "date range.")
+            return action_result.set_status(
+                phantom.APP_ERROR,
+                "Start Date and End Date are both required to search by " "date range.",
+            )
         try:
-            query = self._build_alerts_query(username, start_date, end_date, alert_state)
+            query = self._build_alerts_query(
+                username, start_date, end_date, alert_state
+            )
         except ValueError as exception:
-            return action_result.set_status(phantom.APP_ERROR, f"Start Date and End Date must be in format YYYY-mm-dd: {exception}")
+            return action_result.set_status(
+                phantom.APP_ERROR,
+                f"Start Date and End Date must be in format YYYY-mm-dd: {exception}",
+            )
 
         response = self._client.alerts.search(query)
         action_result.add_data(response.data)
         action_result.update_summary({"total_count": response["totalCount"]})
         return action_result.set_status(phantom.APP_SUCCESS)
+
+    @action_handler_for("set_alert_state")
+    def _handle_set_alert_state(self, param, action_result):
+        alert_id = param["alert_id"]
+        alert_state = param["alert_state"]
+        note = param.get("note")
+        response = self._client.alerts.update_state(alert_state, [alert_id], note=note)
+        action_result.add_data(response.data)
+        status_message = f"State of alert {alert_id} was updated to {alert_state}"
+        action_result.update_summary({"alert_id": alert_id})
+        return action_result.set_status(phantom.APP_SUCCESS, status_message)
 
     def finalize(self):
         # Save the state, this data is saved across actions and app upgrades
@@ -198,7 +233,9 @@ class Code42Connector(BaseConnector):
         return phantom.APP_SUCCESS
 
     def _validate_date_range(self, start_date, end_date):
-        return (not start_date and not end_date) or (start_date is not None and end_date is not None)
+        return (not start_date and not end_date) or (
+            start_date is not None and end_date is not None
+        )
 
     def _build_alerts_query(self, username, start_date, end_date, alert_state):
         filters = []
@@ -206,7 +243,9 @@ class Code42Connector(BaseConnector):
             filters.append(Actor.eq(username))
         if start_date is not None and end_date is not None:
             # This will raise a ValueError if dates not in Y-m-d H:M:S format
-            filters.append(DateObserved.in_range(f"{start_date} 00:00:00", f"{end_date} 00:00:00"))
+            filters.append(
+                DateObserved.in_range(f"{start_date} 00:00:00", f"{end_date} 00:00:00")
+            )
         else:
             # If a date range is not provided, default to returning alerts from last 30 days
             thirty_days_ago = datetime.now() - timedelta(days=30)
@@ -245,6 +284,7 @@ def main():
 
         # User specified a username but not a password, so ask
         import getpass
+
         password = getpass.getpass("Password: ")
 
     csrftoken = None
--- tests/test_alert_actions.py
+++ tests/test_alert_actions.py
@@ -1,7 +1,17 @@
+from unittest import mock
+
+from py42.exceptions import Py42NotFoundError
 from pytest import fixture
 
-from tests.conftest import create_fake_connector, create_mock_response, attach_client, assert_successful_single_data, \
-    assert_successful_summary, assert_fail
+from tests.conftest import (
+    create_fake_connector,
+    create_mock_response,
+    attach_client,
+    assert_successful_single_data,
+    assert_successful_summary,
+    assert_fail,
+    assert_successful_message,
+)
 
 
 def _create_get_alert_details_connector(client):
@@ -14,27 +24,45 @@ def _create_search_alerts_connector(client):
     return attach_client(connector, client)
 
 
+def _create_set_alert_state_connector(client):
+    connector = create_fake_connector("set_alert_state")
+    return attach_client(connector, client)
+
+
 @fixture
-def mock_py42_with_search_alerts_results(mock_py42_client):
-    mock_py42_client.alerts.search.return_value = _MOCK_SEARCH_ALERTS_LIST_RESPONSE
+def mock_py42_with_search_alerts(mocker, mock_py42_client):
+    mock_py42_client.alerts.search.return_value = create_mock_response(
+        mocker, _MOCK_SEARCH_ALERTS_LIST_RESPONSE
+    )
+    return mock_py42_client
+
+
+@fixture
+def mock_py42_with_alert_details(mocker, mock_py42_client):
+    mock_py42_client.alerts.get_details.return_value = create_mock_response(
+        mocker, _MOCK_ALERT_DETAIL_RESPONSE
+    )
 
 
 _MOCK_ALERT_DETAIL_RESPONSE = {
-    "alerts": [{
-        "type$": "ALERT_SUMMARY",
-        "tenantId": "11111111-abcd-4231-99ab-df6434da4663",
-        "type": "FED_COMPOSITE",
-        "name": "Test Alert",
-        "description": "it's a test",
-        "actor": "cool.guy@code42.com",
-        "actorId": "987210998131391466",
-        "target": "N/A",
-        "severity": "LOW",
-        "ruleId": "cab2d5ee-a512-45b1-8848-809327033048",
-        "ruleSource": "Alerting",
-        "id": "11111111-9724-4005-b848-76af488cf5e2",
-        "createdAt": "2021-05-13T16:51:35.4259080Z",
-        "state": "OPEN"}]
+    "alerts": [
+        {
+            "type$": "ALERT_SUMMARY",
+            "tenantId": "11111111-abcd-4231-99ab-df6434da4663",
+            "type": "FED_COMPOSITE",
+            "name": "Test Alert",
+            "description": "it's a test",
+            "actor": "cool.guy@code42.com",
+            "actorId": "987210998131391466",
+            "target": "N/A",
+            "severity": "LOW",
+            "ruleId": "cab2d5ee-a512-45b1-8848-809327033048",
+            "ruleSource": "Alerting",
+            "id": "11111111-9724-4005-b848-76af488cf5e2",
+            "createdAt": "2021-05-13T16:51:35.4259080Z",
+            "state": "OPEN",
+        }
+    ]
 }
 
 _MOCK_SEARCH_ALERTS_LIST_RESPONSE = {
@@ -54,7 +82,7 @@ _MOCK_SEARCH_ALERTS_LIST_RESPONSE = {
             "ruleSource": "Alerting",
             "id": "11111111-9724-4005-b848-76af488cf5e2",
             "createdAt": "2021-05-13T16:51:35.4259080Z",
-            "state": "OPEN"
+            "state": "OPEN",
         },
         {
             "type$": "ALERT_SUMMARY",
@@ -70,19 +98,17 @@ _MOCK_SEARCH_ALERTS_LIST_RESPONSE = {
             "ruleSource": "Alerting",
             "id": "1111111-555f-4880-8909-f5679448e67c",
             "createdAt": "2021-05-13T16:51:35.3465540Z",
-            "state": "OPEN"
-        }
+            "state": "OPEN",
+        },
     ],
     "totalCount": 2,
-    "problems": []
+    "problems": [],
 }
 
 
-
 class TestCode42AlertsConnector(object):
-
     def test_handle_action_when_alert_detail_calls_get_with_expected_args(
-            self, mock_py42_client
+        self, mock_py42_client
     ):
         param = {"alert_id": "123-456-7890"}
         connector = _create_get_alert_details_connector(mock_py42_client)
@@ -92,28 +118,24 @@ class TestCode42AlertsConnector(object):
         mock_py42_client.alerts.get_details.assert_called_once_with([param["alert_id"]])
 
     def test_handle_action_when_alert_detail_adds_response_to_data(
-            self, mocker, mock_py42_client
+        self, mock_py42_with_alert_details
     ):
         param = {"alert_id": "123-456-7890"}
-        response_data = _MOCK_ALERT_DETAIL_RESPONSE
-        mock_py42_client.alerts.get_details.return_value = create_mock_response(mocker, response_data)
-        connector = _create_get_alert_details_connector(mock_py42_client)
-
+        connector = _create_get_alert_details_connector(mock_py42_with_alert_details)
         connector.handle_action(param)
+        assert_successful_single_data(
+            connector, _MOCK_ALERT_DETAIL_RESPONSE["alerts"][0]
+        )
 
-        assert_successful_single_data(connector, response_data["alerts"][0])
-
-    def test_handle_action_when_alert_detail_adds_info_to_summary(self, mocker, mock_py42_client):
+    def test_handle_action_when_alert_detail_adds_info_to_summary(
+        self, mock_py42_with_alert_details
+    ):
         param = {"alert_id": "123-456-7890"}
-        response_data = _MOCK_ALERT_DETAIL_RESPONSE
-        mock_py42_client.alerts.get_details.return_value = create_mock_response(mocker, response_data)
-        connector = _create_get_alert_details_connector(mock_py42_client)
-
+        connector = _create_get_alert_details_connector(mock_py42_with_alert_details)
         connector.handle_action(param)
-
         expected_summary = {
-            "username": response_data["alerts"][0]["actor"],
-            "user_id": response_data["alerts"][0]["actorId"]
+            "username": _MOCK_ALERT_DETAIL_RESPONSE["alerts"][0]["actor"],
+            "user_id": _MOCK_ALERT_DETAIL_RESPONSE["alerts"][0]["actorId"],
         }
         assert_successful_summary(connector, expected_summary)
 
@@ -122,27 +144,31 @@ class TestCode42AlertsConnector(object):
             "username": "gilbert.lequiche@finance.biz",
             "start_date": "2031-01-01",
             "end_date": "2031-06-01",
-            "alert_state": "OPEN"
+            "alert_state": "OPEN",
         }
         connector = _create_search_alerts_connector(mock_py42_client)
         connector.handle_action(param)
         mock_py42_client.alerts.search.assert_called_once()
 
-    def test_handle_action_when_search_alerts_and_all_params_missing_sets_error_message(self, mock_py42_client):
+    def test_handle_action_when_search_alerts_and_all_params_missing_sets_error_message(
+        self, mock_py42_client
+    ):
         param = {}
         connector = _create_search_alerts_connector(mock_py42_client)
         connector.handle_action(param)
         assert_fail(connector)
 
-    def test_handle_action_when_search_alerts_and_date_range_incomplete_sets_error_message(self, mock_py42_client):
-        param = {
-            "start_date": "2031-01-01"
-        }
+    def test_handle_action_when_search_alerts_and_date_range_incomplete_sets_error_message(
+        self, mock_py42_client
+    ):
+        param = {"start_date": "2031-01-01"}
         connector = _create_search_alerts_connector(mock_py42_client)
         connector.handle_action(param)
         assert_fail(connector)
 
-    def test_handle_action_when_search_alerts_and_date_format_invalid_sets_error_message(self, mock_py42_client):
+    def test_handle_action_when_search_alerts_and_date_format_invalid_sets_error_message(
+        self, mock_py42_client
+    ):
         param = {
             "start_date": "2031-31-12",
             "end_date": "2032-31-01",
@@ -151,34 +177,66 @@ class TestCode42AlertsConnector(object):
         connector.handle_action(param)
         assert_fail(connector)
 
-    def test_handle_action_when_search_alerts_adds_response_items_to_data(self, mocker, mock_py42_client):
+    def test_handle_action_when_search_alerts_adds_response_items_to_data(
+        self, mock_py42_with_search_alerts
+    ):
         param = {"username": "barney.frankenberry@chocula.com"}
-        connector = _create_search_alerts_connector(mock_py42_client)
-        mock_py42_client.alerts.search.return_value = create_mock_response(mocker, _MOCK_SEARCH_ALERTS_LIST_RESPONSE)
+        connector = _create_search_alerts_connector(mock_py42_with_search_alerts)
         connector.handle_action(param)
         assert_successful_single_data(connector, _MOCK_SEARCH_ALERTS_LIST_RESPONSE)
 
-    def test_handle_action_when_search_alerts_adds_summary(self, mocker, mock_py42_client):
+    def test_handle_action_when_search_alerts_adds_summary(
+        self, mock_py42_with_search_alerts
+    ):
         param = {"username": "barney.frankenberry@chocula.com"}
-        connector = _create_search_alerts_connector(mock_py42_client)
-        mock_py42_client.alerts.search.return_value = create_mock_response(mocker, _MOCK_SEARCH_ALERTS_LIST_RESPONSE)
+        connector = _create_search_alerts_connector(mock_py42_with_search_alerts)
         connector.handle_action(param)
-        assert_successful_summary(connector, {"total_count": _MOCK_SEARCH_ALERTS_LIST_RESPONSE["totalCount"]})
+        assert_successful_summary(
+            connector, {"total_count": _MOCK_SEARCH_ALERTS_LIST_RESPONSE["totalCount"]}
+        )
 
-    def test_handle_action_when_search_alerts_and_no_date_range_provided_defaults_to_last_30_days(self, mock_py42_client):
+    def test_handle_action_when_search_alerts_and_no_date_range_provided_defaults_to_last_30_days(
+        self, mock_py42_client
+    ):
         param = {"username": "barney.frankenberry@chocula.com"}
         connector = _create_search_alerts_connector(mock_py42_client)
         connector.handle_action(param)
         actual_query = mock_py42_client.alerts.search.call_args[0][0]
+        # This is not a great assertion...
         assert len(actual_query._filter_group_list) == 2
 
+    def test_handle_action_when_set_alert_state_calls_update_state(
+        self, mock_py42_client
+    ):
+        param = {"alert_id": "1234-5678-0000", "alert_state": "RESOLVED"}
+        connector = _create_set_alert_state_connector(mock_py42_client)
+        connector.handle_action(param)
+        mock_py42_client.alerts.update_state.assert_called_once_with(
+            param["alert_state"], [param["alert_id"]], note=None
+        )
 
+    def test_handle_action_when_set_alert_state_sets_message(self, mock_py42_client):
+        param = {"alert_id": "1234-5678-0000", "alert_state": "RESOLVED"}
+        connector = _create_set_alert_state_connector(mock_py42_client)
+        connector.handle_action(param)
+        expected_message = (
+            f"State of alert {param['alert_id']} was updated to {param['alert_state']}"
+        )
+        assert_successful_message(connector, expected_message)
 
+    def test_handle_action_when_set_alert_state_updates_summary(self, mock_py42_client):
+        param = {"alert_id": "1234-5678-0000", "alert_state": "RESOLVED"}
+        connector = _create_set_alert_state_connector(mock_py42_client)
+        connector.handle_action(param)
+        assert_successful_summary(connector, {"alert_id": param["alert_id"]})
 
-
-
-
-
-
-
-
+    def test_handle_action_when_set_alert_state_and_alert_doesnt_exist_sets_error_message(
+        self, mock_py42_client
+    ):
+        param = {"alert_id": "1234-doesnt-exist-5678", "alert_state": "OPEN"}
+        connector = _create_set_alert_state_connector(mock_py42_client)
+        mock_py42_client.alerts.update_state.side_effect = Py42NotFoundError(
+            mock.Mock(status=404)
+        )
+        connector.handle_action(param)
+        assert_fail(connector)
