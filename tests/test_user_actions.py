01adf5ea4cb298e43f734cdc6d29e17f45d5263e
Peter Briggs <peter.briggs@code42.com>
2021-06-17 09:14:47 -0500

    add a couple tests

--- tests/test_user_actions.py
+++ tests/test_user_actions.py
@@ -21,6 +21,30 @@ _TEST_FIRSTNAME = "test_first"
 _TEST_LASTNAME = "test_last"
 _TEST_NOTE = "noting a test"
 
+_MOCK_USER_PROFILE_RESPONSE = {
+    "city": "Minneapolis",
+    "cloudUsernames": ["test@example.com"],
+    "country": "US",
+    "department": "Sales",
+    "displayName": "Jim Harper",
+    "managerDisplayName": "Jordan Anderson",
+    "managerUid": "886765398677810428",
+    "managerUsername": "test2@example.com",
+    "riskFactors": [
+        "HIGH_IMPACT_EMPLOYEE",
+        "ELEVATED_ACCESS_PRIVILEGES",
+        "PERFORMANCE_CONCERNS",
+        "FLIGHT_RISK",
+        "SUSPICIOUS_SYSTEM_ACTIVITY",
+    ],
+    "state": "MN",
+    "tenantId": "111111-824a-40a3-a6d9-345664cfbb33",
+    "title": "Regional Vice President",
+    "type$": "USER_V2",
+    "userId": "886933071206061686",
+    "userName": "test@example.com",
+}
+
 
 @fixture
 def mock_py42_with_create_user(mocker, mock_py42_client):
@@ -38,6 +62,14 @@ def mock_py42_with_legacy_id_user(mocker, mock_py42_client):
     }
     mock_py42_client.users.get_by_username.return_value = create_mock_response(
         mocker, response_data
+    )
+    return mock_py42_client
+
+
+@fixture
+def mock_py42_with_user_profile(mocker, mock_py42_client):
+    mock_py42_client.detectionlists.get_user.return_value = create_mock_response(
+        mocker, _MOCK_USER_PROFILE_RESPONSE
     )
     return mock_py42_client
 
@@ -93,14 +125,15 @@ class TestCode42UsersConnector(object):
         connector = create_fake_connector("create_user", mock_py42_with_create_user)
         connector.handle_action(param)
         assert_successful_single_data(
-            connector, {"userUid": _TEST_USER_UID, "userId": _TEST_USER_LEGACY_ID},
+            connector,
+            {"userUid": _TEST_USER_UID, "userId": _TEST_USER_LEGACY_ID},
         )
 
     def test_handle_action_when_creating_user_sets_error_status_when_duplicate_user(
         self, mock_py42_with_create_user
     ):
-        mock_py42_with_create_user.users.create_user.side_effect = Py42UserAlreadyExistsError(
-            mock.Mock(status=500)
+        mock_py42_with_create_user.users.create_user.side_effect = (
+            Py42UserAlreadyExistsError(mock.Mock(status=500))
         )
         param = {"org_uid": _TEST_ORG_UID, "username": _TEST_USERNAME}
         connector = create_fake_connector("create_user", mock_py42_with_create_user)
@@ -229,3 +262,24 @@ class TestCode42UsersConnector(object):
         )
         connector.handle_action(param)
         assert_fail(connector)
+
+    def test_handle_action_when_get_user_profile_calls_with_expected_args(
+        self, mock_py42_with_user_profile
+    ):
+        param = {"username": _TEST_USERNAME}
+        connector = create_fake_connector(
+            "get_user_profile", mock_py42_with_user_profile
+        )
+        connector.handle_action(param)
+        mock_py42_with_user_profile.detectionlists.get_user.assert_called_once_with(
+            _TEST_USERNAME
+        )
+
+    def test_handle_action_when_get_user_profile_adds_response_to_data(self, mock_py42_with_user_profile):
+        param = {"username": _TEST_USERNAME}
+        connector = create_fake_connector("get_user_profile", mock_py42_with_user_profile)
+        connector.handle_action(param)
+        assert_successful_single_data(
+            connector,
+            _MOCK_USER_PROFILE_RESPONSE,
+        )
